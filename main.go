38def2f9fa324801ec8a5ebf0d5d5c9df5f2bcb5
PumpkinSeed <qwer.kocka@gmail.com>
2023-12-28 15:09:38 +0100

    Add httplistener

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/fatih/color"
 )
@@ -14,9 +19,48 @@ const (
 	defaultHost = ":8177"
 )
 
-var tab = "    "
+var (
+	host     = defaultHost
+	output   = "terminal"
+	filepath = ""
+
+	outputs = map[string]func(r *http.Request, data []byte){
+		"terminal":      outputTerminal,
+		"terminal-json": outputTerminalJSON,
+		"file":          outputFile,
+		"file-json":     outputFileJSON,
+	}
+)
+
+func init() {
+	flag.StringVar(&host, "host", defaultHost, "Defines the host of the service. (default: :8177)")
+	flag.StringVar(&output, "output", "terminal", "Defines the output(s), separated with semicolon. (default: terminal)")
+	flag.StringVar(&filepath, "filepath", "", "Defines the filepath to the file output.")
+	flag.Parse()
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	data, err := io.ReadAll(r.Body)
+	e(err)
+	for _, o := range strings.Split(output, ";") {
+		outputs[o](r, data)
+	}
+	_, err = io.WriteString(w, "ACK")
+	e(err)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+
+	host := os.Getenv(envHost)
+	if host == "" {
+		host = defaultHost
+	}
+	color.HiMagenta(">>> Server listening on %s", host)
+	panic(http.ListenAndServe(host, nil))
+}
+
+func outputTerminal(r *http.Request, data []byte) {
 	color.Cyan("_________________________________________________________________________________________________")
 	color.Cyan("| Incoming request")
 	color.HiCyan(t(1)+"%s %s", r.Method, r.Host+r.URL.Path)
@@ -29,13 +73,89 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	color.Green(t(1) + "--------------------")
 	color.Green(t(1) + "Data")
-	data, err := io.ReadAll(r.Body)
-	e(err)
 	color.HiGreen(t(2)+"%s", data)
-	io.WriteString(w, "ACK")
+}
+
+func outputTerminalJSON(r *http.Request, data []byte) {
+	var j = requestToJSON(r, data)
+	result, err := json.Marshal(j)
+	e(err)
+	fmt.Println(string(result))
+}
+
+func outputFile(r *http.Request, data []byte) {
+	f := openFile()
+	if f == nil {
+		return
+	}
+	var builder strings.Builder
+	builder.WriteString("_________________________________________________________________________________________________\n")
+	builder.WriteString("| Incoming request\n")
+	builder.WriteString(fmt.Sprintf(t(1)+"%s %s\n", r.Method, r.Host+r.URL.Path))
+
+	builder.WriteString(t(1) + "--------------------\n")
+	builder.WriteString(t(1) + "Headers\n")
+	for k, v := range r.Header {
+		builder.WriteString(fmt.Sprintf(t(2)+"%s %v\n", k, v))
+	}
+
+	builder.WriteString(t(1) + "--------------------\n")
+	builder.WriteString(t(1) + "Data\n")
+	builder.WriteString(fmt.Sprintf(t(2)+"%s\n", data))
+	_, err := fmt.Fprint(f, builder.String())
+	e(err)
+}
+
+func outputFileJSON(r *http.Request, data []byte) {
+	f := openFile()
+	if f == nil {
+		return
+	}
+	var j = requestToJSON(r, data)
+	result, err := json.Marshal(j)
+	e(err)
+	_, err = fmt.Fprintln(f, string(result))
+	e(err)
+}
+
+func openFile() *os.File {
+	var f *os.File
+	var err error
+	if _, err = os.Stat(filepath); err == nil {
+		f, err = os.Open(filepath)
+		if err != nil {
+			e(err)
+			return nil
+		}
+
+	} else if errors.Is(err, os.ErrNotExist) {
+		f, err = os.Create(filepath)
+		if err != nil {
+			e(err)
+			return nil
+		}
+	} else {
+		e(err)
+		return nil
+	}
+	return f
+}
+
+func requestToJSON(r *http.Request, data []byte) map[string]interface{} {
+	var result = make(map[string]interface{})
+	result["method"] = r.Method
+	result["path"] = r.Host + r.URL.Path
+	result["headers"] = r.Header
+	var dataContainer = make(map[string]interface{})
+	err := json.Unmarshal(data, &dataContainer)
+	e(err)
+	result["body"] = dataContainer
+
+	return result
 }
 
 func t(num int) string {
+	var tab = "    "
 	var result = "| "
 	for i := 0; i < num; i++ {
 		result += tab
@@ -47,15 +167,4 @@ func e(err error) {
 	if err != nil {
 		color.Red(">>> %s", err.Error())
 	}
-}
-
-func main() {
-	http.HandleFunc("/", handler)
-
-	host := os.Getenv(envHost)
-	if host == "" {
-		host = defaultHost
-	}
-	color.HiMagenta(">>> Server listening on %s", host)
-	panic(http.ListenAndServe(host, nil))
 }
